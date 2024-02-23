5945342c6516122cbddb42bb4cd93abf822e782d
John Doe <hugosum.dev@protonmail.com>
2024-02-23 06:34:53 +0000

    query all subtype at bql level

--- src/adapters/surrealDB/index.ts
+++ src/adapters/surrealDB/index.ts
@@ -1,5 +1,5 @@
 import type { Pipeline } from '../../types/pipeline/base'
-import type { BaseResponse, PipelineOperation } from '../../types'
+import type { BaseResponse, EnrichedBormSchema, PipelineOperation } from '../../types'
 import { enrichBQLQuery } from '../../pipeline/preprocess/query/enrichBQLQuery';
 import { postHooks } from '../../pipeline/postprocess/query/postHooks';
 import { cleanQueryRes } from '../../pipeline/postprocess/query/cleanQueryRes';
@@ -8,40 +8,66 @@ type SurrealDbResponse = {
 
 } & BaseResponse;
 
+const getSubtype = (schema: EnrichedBormSchema, kind: "entities" | "relations", thing: string, result: Array<string> = []): Array<string> => {
+  const subtypes = Object.values(schema[kind])
+    .filter((itemSchema) => itemSchema.extends === thing)
+    .map((itemSchema) => itemSchema.name)
+
+  if (subtypes.length === 0) return result
+
+  for (const subtype of subtypes) {
+    result.push(subtype)
+    getSubtype(schema, kind, subtype, result)
+  }
+
+  return result
+}
+
 const buildSurrealDbQuery: PipelineOperation<SurrealDbResponse> = async (req, res) => {
-  const { dbHandles, enrichedBqlQuery } = req;
-	if (!enrichedBqlQuery) {
-		throw new Error('BQL request not parsed');
-	}
-  if(!dbHandles.surrealDB){
+  const { dbHandles, enrichedBqlQuery, schema } = req;
+
+  console.log('what is schema', schema)
+
+  if (!enrichedBqlQuery) {
+    throw new Error('BQL request not parsed');
+  }
+  if (!dbHandles.surrealDB) {
     throw new Error('missing SurrealDB in dbHandles');
   }
 
   const connector = req.config.dbConnectors.find((connector) => connector.provider === "surrealDB")
 
-  if(!connector){
+  if (!connector) {
     throw new Error('missing SurrealDB config')
   }
 
   const mapItem = dbHandles.surrealDB.get(connector.id)
 
-  if(!mapItem){
+  if (!mapItem) {
     throw new Error(`missing SurrealDB client with id of ${connector.id}`)
   }
 
   const { client } = mapItem
 
+  // @ts-expect-error enrichedBqlQuery is any
+  const allEntities = req.enrichedBqlQuery.map((query) => {
+    if (query["$thingType"] !== "entity") {
+      throw new Error('unimplemented')
+    }
 
-  for(const query of req.enrichedBqlQuery) {
-    console.log('check query', query)
-  }
+    return [query['$thing'], ...getSubtype(req.schema, query["$thingType"] === "entity" ? "entities" : "relations", query['$thing'])]
+  })
+
+  console.log('check entities', allEntities)
+
+  res.bqlRes = []
 }
 
 export const SurrealDbPipelines: Record<string, Pipeline<SurrealDbResponse>> = {
   query: [enrichBQLQuery,
     buildSurrealDbQuery,
-    // postHooks,
-    // cleanQueryRes
+    postHooks,
+    cleanQueryRes
   ],
   mutation: [
 
--- tests/surrealdb/unit/queries/query.test.ts
+++ tests/surrealdb/unit/queries/query.test.ts
@@ -40,92 +40,92 @@ describe('Query', () => {
 	// 	await expect(bormClient.query({ $entity: 'fakeEntity' })).rejects.toThrow();
 	// });
 
-	it('v3[validation] - $id not existing', async () => {
-		expect(bormClient).toBeDefined();
-		const res = await bormClient.query({ $entity: 'User', $id: 'nonExisting' });
-		await expect(res).toBeNull();
-	});
-
-	// it('e1[entity] - basic and direct link to relation', async () => {
+	// it('v3[validation] - $id not existing', async () => {
 	// 	expect(bormClient).toBeDefined();
-	// 	const query = { $entity: 'User' };
-	// 	const expectedRes = [
-	// 		{
-	// 			$id: 'god1',
-	// 			$thing: 'God',
-	// 			$thingType: 'entity',
-	// 			email: 'afx@rephlex.com',
-	// 			id: 'god1',
-	// 			name: 'Richard David James',
-	// 		},
-	// 		{
-	// 			$id: 'superuser1',
-	// 			$thing: 'SuperUser',
-	// 			$thingType: 'entity',
-	// 			email: 'black.mamba@deadly-viper.com',
-	// 			id: 'superuser1',
-	// 			name: 'Beatrix Kiddo',
-	// 		},
-	// 		{
-	// 			// '$entity': 'User',
-	// 			'$thing': 'User',
-	// 			'$thingType': 'entity',
-	// 			'$id': 'user1',
-	// 			'name': 'Antoine',
-	// 			'email': 'antoine@test.com',
-	// 			'id': 'user1',
-	// 			'accounts': ['account1-1', 'account1-2', 'account1-3'],
-	// 			'spaces': ['space-1', 'space-2'],
-	// 			'user-tags': ['tag-1', 'tag-2'],
-	// 		},
-	// 		{
-	// 			// '$entity': 'User',
-	// 			'$thing': 'User',
-	// 			'$thingType': 'entity',
-	// 			'$id': 'user2',
-	// 			'name': 'Loic',
-	// 			'email': 'loic@test.com',
-	// 			'id': 'user2',
-	// 			'accounts': ['account2-1'],
-	// 			'spaces': ['space-2'],
-	// 			'user-tags': ['tag-3', 'tag-4'],
-	// 		},
-	// 		{
-	// 			// '$entity': 'User',
-	// 			'$thing': 'User',
-	// 			'$thingType': 'entity',
-	// 			'$id': 'user3',
-	// 			'name': 'Ann',
-	// 			'email': 'ann@test.com',
-	// 			'id': 'user3',
-	// 			'accounts': ['account3-1'],
-	// 			'spaces': ['space-2'],
-	// 			'user-tags': ['tag-2'],
-	// 		},
-	// 		{
-	// 			// $entity: 'User',
-	// 			$thing: 'User',
-	// 			$thingType: 'entity',
-	// 			$id: 'user4',
-	// 			id: 'user4',
-	// 			name: 'Ben',
-	// 		},
-	// 		{
-	// 			// $entity: 'User',
-	// 			$thing: 'User',
-	// 			$thingType: 'entity',
-	// 			$id: 'user5',
-	// 			email: 'charlize@test.com',
-	// 			id: 'user5',
-	// 			name: 'Charlize',
-	// 			spaces: ['space-1'],
-	// 		},
-	// 	];
-	// 	const res = await bormClient.query(query);
-	// 	expect(res).toBeDefined();
-	// 	expect(res).not.toBeInstanceOf(String);
-	// 	expect(deepSort(res, 'id')).toEqual(expectedRes);
+	// 	const res = await bormClient.query({ $entity: 'User', $id: 'nonExisting' });
+	// 	await expect(res).toBeNull();
 	// });
+
+	it('e1[entity] - basic and direct link to relation', async () => {
+		expect(bormClient).toBeDefined();
+		const query = { $entity: 'User' };
+		const expectedRes = [
+			{
+				$id: 'god1',
+				$thing: 'God',
+				$thingType: 'entity',
+				email: 'afx@rephlex.com',
+				id: 'god1',
+				name: 'Richard David James',
+			},
+			{
+				$id: 'superuser1',
+				$thing: 'SuperUser',
+				$thingType: 'entity',
+				email: 'black.mamba@deadly-viper.com',
+				id: 'superuser1',
+				name: 'Beatrix Kiddo',
+			},
+			{
+				// '$entity': 'User',
+				'$thing': 'User',
+				'$thingType': 'entity',
+				'$id': 'user1',
+				'name': 'Antoine',
+				'email': 'antoine@test.com',
+				'id': 'user1',
+				'accounts': ['account1-1', 'account1-2', 'account1-3'],
+				'spaces': ['space-1', 'space-2'],
+				'user-tags': ['tag-1', 'tag-2'],
+			},
+			{
+				// '$entity': 'User',
+				'$thing': 'User',
+				'$thingType': 'entity',
+				'$id': 'user2',
+				'name': 'Loic',
+				'email': 'loic@test.com',
+				'id': 'user2',
+				'accounts': ['account2-1'],
+				'spaces': ['space-2'],
+				'user-tags': ['tag-3', 'tag-4'],
+			},
+			{
+				// '$entity': 'User',
+				'$thing': 'User',
+				'$thingType': 'entity',
+				'$id': 'user3',
+				'name': 'Ann',
+				'email': 'ann@test.com',
+				'id': 'user3',
+				'accounts': ['account3-1'],
+				'spaces': ['space-2'],
+				'user-tags': ['tag-2'],
+			},
+			{
+				// $entity: 'User',
+				$thing: 'User',
+				$thingType: 'entity',
+				$id: 'user4',
+				id: 'user4',
+				name: 'Ben',
+			},
+			{
+				// $entity: 'User',
+				$thing: 'User',
+				$thingType: 'entity',
+				$id: 'user5',
+				email: 'charlize@test.com',
+				id: 'user5',
+				name: 'Charlize',
+				spaces: ['space-1'],
+			},
+		];
+		const res = await bormClient.query(query);
+		expect(res).toBeDefined();
+		expect(res).not.toBeInstanceOf(String);
+		expect(deepSort(res, 'id')).toEqual(expectedRes);
+	});
 
 	afterAll(async () => {
 		await cleanup(bormClient, dbName);
