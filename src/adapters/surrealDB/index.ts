9d2e6a42e1cceaff050512f18afcc6b20a3607bd
John Doe <hugosum.dev@protonmail.com>
2024-02-23 14:56:24 +0000

    incomplete

--- src/adapters/surrealDB/index.ts
+++ src/adapters/surrealDB/index.ts
@@ -23,10 +23,71 @@ const getSubtype = (schema: EnrichedBormSchema, kind: "entities" | "relations", 
   return result
 }
 
+type EnrichedBqlQuery = {
+  '$path': string,
+  '$thing': string,
+  '$thingType': string
+  '$fields': Array<{
+    '$path': string,
+    '$dbPath': string,
+    '$thingType': 'attribute'
+    '$as': string,
+    '$var': string,
+    '$fieldType': 'data',
+    '$excludedFields': undefined,
+    '$justId': boolean,
+    '$id': undefined,
+    '$filter': undefined,
+    '$isVirtual': undefined,
+    '$filterProcessed': boolean,
+  } | {
+    '$thingType': 'entity',
+    '$plays': string,
+    '$playedBy': {
+      path: string,
+      cardinality: string,
+      relation: string,
+      plays: string,
+      target: string,
+      thing: string,
+      thingType:string,
+    },
+    '$path': string,
+    '$dbPath': undefined,
+    '$as': string,
+    '$var': string,
+    '$thing': string,
+    '$fields': Array<any>
+    '$excludedFields': undefined,
+    '$fieldType': string,
+    '$target': string,
+    '$intermediary': string,
+    '$justId': boolean
+    '$id': undefined,
+    '$filter': {},
+    '$idNotIncluded': boolean,
+    '$filterByUnique': boolean,
+    '$filterProcessed': boolean
+  }>
+}
+
+// any now, wait for type from enrichedBqlQuery
+const buildQuery = (query: EnrichedBqlQuery) => {
+
+  const attributes =  query["$fields"].filter((q) => q["$thingType"] === "attribute")
+
+  // console.log('hi', attributes.map((attr) => `${attr['$path']}` ).join(','))
+
+  const entities = query["$fields"].filter((q) => q["$thingType"] === "entity")
+
+  console.log('bye entities', entities)
+
+  // console.log('hi entities', JSON.stringify(entities, null, 2))
+
+}
+
 const buildSurrealDbQuery: PipelineOperation<SurrealDbResponse> = async (req, res) => {
   const { dbHandles, enrichedBqlQuery, schema } = req;
-
-  console.log('what is schema', schema)
 
   if (!enrichedBqlQuery) {
     throw new Error('BQL request not parsed');
@@ -55,12 +116,30 @@ const buildSurrealDbQuery: PipelineOperation<SurrealDbResponse> = async (req, re
       throw new Error('unimplemented')
     }
 
+    // console.log('what is schema', schema)
+
+    console.log('byeee query', buildQuery(query))
+
     return [query['$thing'], ...getSubtype(req.schema, query["$thingType"] === "entity" ? "entities" : "relations", query['$thing'])]
   })
 
-  console.log('check entities', allEntities)
+  if(allEntities.length > 1){
+    throw new Error('multiple qureies unimplemented')
+  }
 
-  res.bqlRes = []
+  const queryRes = await Promise.all(allEntities[0].map(async (entity: string) => {
+    return await client.query(`
+    SELECT
+      *,
+      <-SpaceUser_users<-SpaceUser->SpaceUser_spaces.out.id as spaces,
+      <-UserAccounts_user<-UserAccounts->UserAccounts_accounts.out as accounts
+    FROM ${entity} FETCH Space, Account PARALLEL;
+    `)
+  }))
+
+  const result = queryRes.flat()
+
+  res.bqlRes = result
 }
 
 export const SurrealDbPipelines: Record<string, Pipeline<SurrealDbResponse>> = {
