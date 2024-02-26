f591fe4a4274ad4cac7d4da6812974119031c4d6
John Doe <hugosum.dev@protonmail.com>
2024-02-26 14:13:20 +0000

    before fixing case

--- tests/surrealdb/unit/queries/query.test.ts
+++ tests/surrealdb/unit/queries/query.test.ts
@@ -46,97 +46,97 @@ describe('Query', () => {
 		await expect(res).toEqual(undefined)
 	});
 
-	// it('e1[entity] - basic and direct link to relation', async () => {
-	// 	expect(bormClient).toBeDefined();
-	// 	const query = { $entity: 'User' };
-	// 	const expectedRes = [
-	// 		{
-	// 			$id: 'god1',
-	// 			$thing: 'God',
-	// 			$thingType: 'entity',
-	// 			email: 'afx@rephlex.com',
-	// 			id: 'god1',
-	// 			name: 'Richard David James',
-  //       'accounts': [],
-	// 			'spaces': [],
-	// 			'user-tags': [],
-	// 		},
-	// 		{
-	// 			$id: 'superuser1',
-	// 			$thing: 'SuperUser',
-	// 			$thingType: 'entity',
-	// 			email: 'black.mamba@deadly-viper.com',
-	// 			id: 'superuser1',
-	// 			name: 'Beatrix Kiddo',
-  //       'accounts': [],
-	// 			'spaces': [],
-	// 			'user-tags': [],
-	// 		},
-	// 		{
-	// 			// '$entity': 'User',
-	// 			'$thing': 'User',
-	// 			'$thingType': 'entity',
-	// 			'$id': 'user1',
-	// 			'name': 'Antoine',
-	// 			'email': 'antoine@test.com',
-	// 			'id': 'user1',
-	// 			'accounts': ['account1-1', 'account1-2', 'account1-3'],
-	// 			'spaces': ['space-1', 'space-2'],
-	// 			'user-tags': ['tag-1', 'tag-2'],
-	// 		},
-	// 		{
-	// 			// '$entity': 'User',
-	// 			'$thing': 'User',
-	// 			'$thingType': 'entity',
-	// 			'$id': 'user2',
-	// 			'name': 'Loic',
-	// 			'email': 'loic@test.com',
-	// 			'id': 'user2',
-	// 			'accounts': ['account2-1'],
-	// 			'spaces': ['space-2'],
-	// 			'user-tags': ['tag-3', 'tag-4'],
-	// 		},
-	// 		{
-	// 			// '$entity': 'User',
-	// 			'$thing': 'User',
-	// 			'$thingType': 'entity',
-	// 			'$id': 'user3',
-	// 			'name': 'Ann',
-	// 			'email': 'ann@test.com',
-	// 			'id': 'user3',
-	// 			'accounts': ['account3-1'],
-	// 			'spaces': ['space-2'],
-	// 			'user-tags': ['tag-2'],
-	// 		},
-	// 		{
-	// 			// $entity: 'User',
-	// 			$thing: 'User',
-	// 			$thingType: 'entity',
-	// 			$id: 'user4',
-	// 			id: 'user4',
-	// 			name: 'Ben',
-  //       'accounts': [],
-	// 			'spaces': [],
-	// 			'user-tags': [],
-	// 		},
-	// 		{
-	// 			// $entity: 'User',
-	// 			$thing: 'User',
-	// 			$thingType: 'entity',
-	// 			$id: 'user5',
-	// 			email: 'charlize@test.com',
-	// 			id: 'user5',
-	// 			name: 'Charlize',
-	// 			spaces: ['space-1'],
-  //       'accounts': [],
-	// 			'user-tags': [],
-	// 		},
-	// 	];
-	// 	const res = await bormClient.query(query);
-	// 	expect(res).toBeDefined();
-	// 	expect(res).not.toBeInstanceOf(String);
-	// 	expect(deepSort(res, 'id')).toEqual(expectedRes);
-	// });
+	it('e1[entity] - basic and direct link to relation', async () => {
+		expect(bormClient).toBeDefined();
+		const query = { $entity: 'User' };
+		const expectedRes = [
+			{
+				$id: 'god1',
+				$thing: 'God',
+				$thingType: 'entity',
+				email: 'afx@rephlex.com',
+				id: 'god1',
+				name: 'Richard David James',
+        'accounts': [],
+				'spaces': [],
+				'user-tags': [],
+			},
+			{
+				$id: 'superuser1',
+				$thing: 'SuperUser',
+				$thingType: 'entity',
+				email: 'black.mamba@deadly-viper.com',
+				id: 'superuser1',
+				name: 'Beatrix Kiddo',
+        'accounts': [],
+				'spaces': [],
+				'user-tags': [],
+			},
+			{
+				// '$entity': 'User',
+				'$thing': 'User',
+				'$thingType': 'entity',
+				'$id': 'user1',
+				'name': 'Antoine',
+				'email': 'antoine@test.com',
+				'id': 'user1',
+				'accounts': ['account1-1', 'account1-2', 'account1-3'],
+				'spaces': ['space-1', 'space-2'],
+				'user-tags': ['tag-1', 'tag-2'],
+			},
+			{
+				// '$entity': 'User',
+				'$thing': 'User',
+				'$thingType': 'entity',
+				'$id': 'user2',
+				'name': 'Loic',
+				'email': 'loic@test.com',
+				'id': 'user2',
+				'accounts': ['account2-1'],
+				'spaces': ['space-2'],
+				'user-tags': ['tag-3', 'tag-4'],
+			},
+			{
+				// '$entity': 'User',
+				'$thing': 'User',
+				'$thingType': 'entity',
+				'$id': 'user3',
+				'name': 'Ann',
+				'email': 'ann@test.com',
+				'id': 'user3',
+				'accounts': ['account3-1'],
+				'spaces': ['space-2'],
+				'user-tags': ['tag-2'],
+			},
+			{
+				// $entity: 'User',
+				$thing: 'User',
+				$thingType: 'entity',
+				$id: 'user4',
+				id: 'user4',
+				name: 'Ben',
+        'accounts': [],
+				'spaces': [],
+				'user-tags': [],
+			},
+			{
+				// $entity: 'User',
+				$thing: 'User',
+				$thingType: 'entity',
+				$id: 'user5',
+				email: 'charlize@test.com',
+				id: 'user5',
+				name: 'Charlize',
+				spaces: ['space-1'],
+        'accounts': [],
+				'user-tags': [],
+			},
+		];
+		const res = await bormClient.query(query);
+		expect(res).toBeDefined();
+		expect(res).not.toBeInstanceOf(String);
+		expect(deepSort(res, 'id')).toEqual(expectedRes);
+	});
 
 	it('e1.b[entity] - basic and direct link to relation sub entity', async () => {
 		expect(bormClient).toBeDefined();
@@ -162,28 +162,28 @@ describe('Query', () => {
 		expect(deepSort(res, 'id')).toEqual(expectedRes);
 	});
 
-	// it('e2[entity] - filter by single $id', async () => {
-	// 	expect(bormClient).toBeDefined();
-	// 	const query = { $entity: 'User', $id: 'user1' };
-	// 	const expectedRes = {
-	// 		// '$entity': 'User',
-	// 		'$thing': 'User',
-	// 		'$thingType': 'entity',
-	// 		'$id': 'user1',
-	// 		'name': 'Antoine',
-	// 		'email': 'antoine@test.com',
-	// 		'id': 'user1',
-	// 		'accounts': ['account1-1', 'account1-2', 'account1-3'],
-  //     'sessions': [],
-	// 		'spaces': ['space-1', 'space-2'],
-	// 		'user-tags': ['tag-1', 'tag-2'],
-	// 	};
+	it('e2[entity] - filter by single $id', async () => {
+		expect(bormClient).toBeDefined();
+		const query = { $entity: 'User', $id: 'user1' };
+		const expectedRes = {
+			// '$entity': 'User',
+			'$thing': 'User',
+			'$thingType': 'entity',
+			'$id': 'user1',
+			'name': 'Antoine',
+			'email': 'antoine@test.com',
+			'id': 'user1',
+			'accounts': ['account1-1', 'account1-2', 'account1-3'],
+      'sessions': [],
+			'spaces': ['space-1', 'space-2'],
+			'user-tags': ['tag-1', 'tag-2'],
+		};
 
-	// 	const res = (await bormClient.query(query)) as UserType;
+		const res = (await bormClient.query(query)) as UserType;
 
-	// 	expect(res).toBeDefined();
-	// 	expect(deepSort(res, 'id')).toEqual(expectedRes);
-	// });
+		expect(res).toBeDefined();
+		expect(deepSort(res, 'id')).toEqual(expectedRes);
+	});
 
 	it('opt1[options, noMetadata', async () => {
 		expect(bormClient).toBeDefined();
@@ -209,6 +209,27 @@ describe('Query', () => {
 
 		expect(res['user-tags']).toHaveLength(expectedRes['user-tags'].length);
 	});
+
+	// it('opt3a[options, returnNulll] - empty fields option in entity', async () => {
+	// 	expect(bormClient).toBeDefined();
+	// 	const query = {
+	// 		$entity: 'User',
+	// 		$id: 'user4',
+	// 		$fields: ['spaces', 'email', 'user-tags'],
+	// 	};
+	// 	const expectedRes = {
+	// 		'$thing': 'User',
+	// 		'$thingType': 'entity',
+	// 		'email': null, //Example field
+	// 		'$id': 'user4',
+	// 		'spaces': null, //example linkfield from intermediary relation
+	// 		'user-tags': null, //example linkfield from direct relation
+	// 	};
+	// 	const res = await bormClient.query(query, { returnNulls: true });
+	// 	expect(res).toBeDefined();
+	// 	expect(res).not.toBeInstanceOf(String);
+	// 	expect(deepSort(res, 'id')).toEqual(expectedRes);
+	// });
 
 	afterAll(async () => {
 		await cleanup(bormClient, dbName);
