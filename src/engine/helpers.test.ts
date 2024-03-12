296557deac21ecd00a24546b71f0db09a67b2ab6
Loïc Veillard <l.veillard@gmail.com>
2024-03-12 19:48:56 +0100

    feat: lint + package up

--- src/engine/helpers.test.ts
+++ src/engine/helpers.test.ts
@@ -1,42 +1,41 @@
-import { getDependencies } from "./helpers";
-import acorn from 'acorn';
+import { getDependencies } from './helpers';
 
 test('Should get the dependencies', () => {
-  const fn = (
-    { name }: { name: string },
-    { kinds: unitKinds }: { kinds: string },
-    { spaceId }: { spaceId: string },
-  ) => {
-    console.log(name, unitKinds, spaceId);
-  }
-  const dep = getDependencies(fn);
-  expect(dep).toEqual({
-    current: ['name'],
-    parent: ['kinds'],
-    context: ['spaceId'],
-  });
+	const fn = (
+		{ name }: { name: string },
+		{ kinds: unitKinds }: { kinds: string },
+		{ spaceId }: { spaceId: string },
+	) => {
+		console.log(name, unitKinds, spaceId);
+	};
+	const dep = getDependencies(fn);
+	expect(dep).toEqual({
+		current: ['name'],
+		parent: ['kinds'],
+		context: ['spaceId'],
+	});
 });
 
 test('Should return empty string for unused node params', () => {
-  const fn = () => {
-    console.log();
-  }
-  const dep = getDependencies(fn);
-  expect(dep).toEqual({
-    current: [],
-    parent: [],
-    context: [],
-  });
+	const fn = () => {
+		console.log();
+	};
+	const dep = getDependencies(fn);
+	expect(dep).toEqual({
+		current: [],
+		parent: [],
+		context: [],
+	});
 });
 
 test('Should return empty string for node params that are not destructured', () => {
-  const fn = (current: object, parent: object, context: object) => {
-    console.log(current, parent, context);
-  }
-  const dep = getDependencies(fn);
-  expect(dep).toEqual({
-    current: [],
-    parent: [],
-    context: [],
-  });
+	const fn = (current: object, parent: object, context: object) => {
+		console.log(current, parent, context);
+	};
+	const dep = getDependencies(fn);
+	expect(dep).toEqual({
+		current: [],
+		parent: [],
+		context: [],
+	});
 });
